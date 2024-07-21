31c71f7e2156ada5fc656c341acc9168d9208b90
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-07-20 19:46:43 -0600

    add photolist container component

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,16 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [selected, setSelected] = useState(false);
+  const handleClick = useCallback(()=>{
+    setSelected(!selected);
+  })
   return (
     <div className="photo-list__fav-icon">
+       <div className="photo-list__fav-icon" onClick={handleClick}></div>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+      <FavIcon selected={selected} />
+      </div>
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-
+import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
-
 
 const PhotoListItem = (props) => {
   return (
     <li  className="photo-list__item" >
-      <img className="photo-list__image"  src={props.data.urls.regular} onClick={handleClick}></img>
+      <PhotoFavButton/>
+      <img className="photo-list__image"  src={props.data.urls.regular}></img>
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={props.data.user.profile}></img>
+      <img className="photo-list__user-profile" src={props.data.user.profile}></img>
         <div className="photo-list__user-info">
           <p>{props.data.user.username}</p>
           <p className="photo-list__user-location">
-            <span>{props.data.location.city}</span>, <span>{props.data.location.country}</span>
+            <span>{props.data.location.city}</span>, 
+            <span>{props.data.location.country}</span>
           </p>
         </div>
       </div>
-  </li>
+    </li>
   )
 };
-
 export default PhotoListItem;
