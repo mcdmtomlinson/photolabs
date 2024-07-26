fcbba230db62502929554e5b0737f67d55913e68
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-07-26 11:49:19 -0600

    refactor with useApplicationData with useReducer

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,10 +3,9 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
-  console.log("props when favbutton is clicked", props);
   const handleClick = (state) => {
     const action = {
-      type: 'FAVOURITE',
+      type: 'updateToFavPhotoIds',
       payload: !state[props.id],
       id: props.id
     };
