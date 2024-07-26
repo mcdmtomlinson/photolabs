3ca3aba27124b88d7dd5595b885aacc7547781e1
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-07-25 20:47:52 -0600

    Add Homeroute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import TopicList from './components/TopicList';
 import TopNavigation from 'components/TopNavigationBar';
 import FavBadge from 'components/FavBadge';
 import './App.scss';
+import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <FavBadge />
       </TopNavigation>
       <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
